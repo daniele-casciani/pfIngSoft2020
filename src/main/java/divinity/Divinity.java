0ff997eb564303cea540472870501f21957aa93c
daniele-casciani <52008420+daniele-casciani@users.noreply.github.com>
2020-04-21 12:06:44 +0200

    Update Divinity.java
    Implmentato controllo sui livelli, sono da controllare i match dei tipi.
    NB le segnalazione del compilatore sono dovute a mancati import adeguati.

--- src/main/java/divinity/Divinity.java
+++ src/main/java/divinity/Divinity.java
@@ -1,6 +1,8 @@
 package divinity;
+
 import Game.*;
 import tower.*;
+
 class Divinity {
 	final private boolean threeplayer=true;
 	final private boolean fourplayer=true;
@@ -27,9 +29,17 @@ class Divinity {
 	}
 	
 	public void move(Cell start, Cell end) {
+		Divinity god = new Divinity(gameMap);
 		
-		if(Divinity.isNear(start, end)); //controllo vicinanza della cella 
-		//controllo sui livelli 
+		if(god.isNear(start, end)) { // controllo prima la vicinanza 
+			
+			// controllo movimento di livello 
+			if(god.isNextLevel(start, end) || god.isPreviousLevel(start, end)) {
+				// se entro qui posso chiamare lo spostamento
+			}
+			//mossa non valida 
+		}
+		//mossa non valida
 	}
 	
 	public void win() {
@@ -57,7 +67,7 @@ class Divinity {
 		//salvo le coordinate delle celle 
 		for(int i = 0; i < 5; i++) {
 			for(int j = 0; j < 5; j++) {
-				if(start.equals(gameMap.getCell(i,j))){
+				if(start.equals(gameMap.getCell(i,j))){ // getCell in map da implementare
 					xs = i;
 					ys = j;
 				}
@@ -95,4 +105,33 @@ class Divinity {
 		else return false;
 	}
 	
+	public boolean isSameLevel(Level start, Level end) {
+		if(start.getClass() == end.getClass()) return true;
+	}
+	
+	public boolean isNextLevel(Level start, Level end) { //controllo sul movimento verso l'alto
+		
+		if(end instanceof Dome) return false;
+		
+		else {
+			if(start instanceof Cell && end instanceof TowerL0) return true;
+			if(start instanceof TowerL0 && end instanceof TowerL1) return true;
+			if(start instanceof TowerL1 && end instanceof TowerL2) return true;
+		}
+		
+		return false;
+	}
+	
+	public boolean isPreviousLevel(Level start, Level end) {// controllo il movimento verso il basso 
+		
+		if(end instanceof Dome) return false;
+		
+		else {
+			if(start instanceof TowerL2 && end instanceof TowerL1) return true;
+			if(start instanceof TowerL1&& end instanceof TowerL0) return true;
+			if(start instanceof TowerL0 && end instanceof Cell) return true;
+		}
+		
+		return false;
+	}
 }
