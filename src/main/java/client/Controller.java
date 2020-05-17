b62b433efabfd69d3dfa0d0fca7732b99d7bf6e4
lucatoppio <48562962+lucatoppio@users.noreply.github.com>
2020-05-17 19:23:38 +0200

    GUI update

--- src/main/java/client/Controller.java
+++ src/main/java/client/Controller.java
@@ -4,9 +4,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 import javafx.application.*;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.*;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import utils.*;
@@ -46,26 +53,75 @@ public class Controller extends  Application implements ClientController{
 
 	@Override
 	public void addConstructor(int x, int y) {
-		// TODO Auto-generated method stub
-		
+		GameController cont = ploader.getController();
+		ImageView node = new ImageView("/image/builder.png");
+		node.setFitHeight(80);
+		node.setFitWidth(80);
+		cont.addElement(node, x, y);
 	}
 
 	@Override
 	public void construction(int x, int y, int z) {
-		// TODO Auto-generated method stub
-		
+		GameController cont = ploader.getController();
+		cont.clearCell(x, y);
+		if (z>0|z<5) {
+			Rectangle lv1 = new Rectangle();
+			lv1.setWidth(90);
+			lv1.setHeight(90);
+			lv1.setArcHeight(5);
+			lv1.setArcWidth(5);
+			lv1.setFill(Color.rgb(160,160,160));
+			cont.addElement(lv1, x, y);
+			if(z>1) {
+				Rectangle lv2 = new Rectangle();
+				lv2.setWidth(70);
+				lv2.setHeight(70);
+				lv2.setArcHeight(5);
+				lv2.setArcWidth(5);
+				lv2.setFill(Color.rgb(210,210,210));
+				cont.addElement(lv2, x, y);
+				if(z>2) {
+					Rectangle lv3 = new Rectangle();
+					lv3.setWidth(50);
+					lv3.setHeight(50);
+					lv3.setArcHeight(5);
+					lv3.setArcWidth(5);
+					lv3.setFill(Color.rgb(240,240,240));
+					cont.addElement(lv3, x, y);
+					if(z==4) {
+						Circle dome = new Circle(20);
+						dome.setFill(Color.BLUE);
+						cont.addElement(dome, x, y);
+					}		
+				}
+			}
+		}
 	}
 
 	@Override
 	public void endLoser(Loser loser) {
-		// TODO Auto-generated method stub
-		
+		AnchorPane an = new AnchorPane();
+		Label label = new Label();
+		label.setText("you win");
+		label.setAlignment(Pos.CENTER);
+		label.setFont(new Font(40));
+		Sstage.setScene(new Scene(an));
+		Sstage.showAndWait();
+		Thread.currentThread().interrupt();
+		return;
 	}
 
 	@Override
 	public void endWinner(Winner winner) {
-		// TODO Auto-generated method stub
-		
+		AnchorPane an = new AnchorPane();
+		Label label = new Label();
+		label.setText("you lose");
+		label.setAlignment(Pos.CENTER);
+		label.setFont(new Font(40));
+		Sstage.setScene(new Scene(an));
+		Sstage.showAndWait();
+		Thread.currentThread().interrupt();
+		return;
 	}
 
 	@Override
@@ -125,7 +181,7 @@ public class Controller extends  Application implements ClientController{
 
 	@Override
 	public void boolChoice(String string) throws IOException {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("scelta.fxml"));
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/scelta.fxml"));
 		AnchorPane an = loader.load();
 		Scene scene = new Scene(an,220,100);
 		Sstage.initModality(Modality.APPLICATION_MODAL);
--- src/main/java/client/GameController.java
+++ src/main/java/client/GameController.java
@@ -2,15 +2,19 @@ package client;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
 public class GameController {
-
+	
+	int x1; int y1; int x2; int y2;
+	
     @FXML
     private TextFlow text;
     @FXML
@@ -19,24 +23,53 @@ public class GameController {
     private TextField textinput;
     @FXML
     private Button okbutton;
+    
+    void initialize(){
+    	textinput.clear();
+    	text.getChildren().clear();
+    	text.getChildren().add(new Label("in attesa del login"));
+    	text.getChildren().add(new Text(System.lineSeparator()));
+    	grid.getChildren().clear();
+    }
 
     public void setText(String message) {
 		text.getChildren().add(new Label(message));	
+		text.getChildren().add(new Text(System.lineSeparator()));
 	}
     
 
 	public String getTextInput() {
 		return textinput.getText();
 	}
+	
+	public void addElement(Node node,int x, int y) {
+		grid.add(node, x, y);
+	}
+	
+	public void clearCell(int x, int y) {
+		for(Node nd : grid.getChildren()) {
+			if (GridPane.getRowIndex(nd)==y && GridPane.getColumnIndex(nd)==x) {
+				grid.getChildren().remove(nd);
+			}
+		}
+	}
     
     @FXML
     void dragEnd(DragEvent event) {
+    	//TODO
     }
     @FXML
     void dragStart(MouseEvent event) {
+    	//TODO
     }
     @FXML
-    void passturn(ActionEvent event) {
-
+    void passturn(ActionEvent event) {	
+    	//TODO
     }
+    @FXML
+    void click(MouseEvent event) {
+    	//TODO
+    	x1 = (int)event.getX();
+    }
+    
 }
